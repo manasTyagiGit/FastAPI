5c80373c7ec3323d5cbcef5247bb98d8dd969571
manasTyagiGit <manastyagi.ind@gmail.com>
2025-07-27 18:37:36 +0530

    Add pydantic learning

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+
+# for schema validation
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# class for extension of BaseModel
+class Body (BaseModel) :
+    title: str
+    content: str
+    #Defaultable optional
+    published: bool = True
+    #Truly Optional
+    rating: Optional[int] = None
 
 
 @app.get("/")
@@ -10,3 +24,9 @@ def first_api() :
 @app.get("/getposts")
 def get_posts() :
     return {"Hey user_name" : "These are your posts"}
+
+@app.post("/createposts")
+def create_posts(post_body : Body) :
+    print (post_body)
+    print (post_body.model_dump())
+    return {"post_data" : post_body}
