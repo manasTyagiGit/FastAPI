e3cf0b91ee759ea3bb50a20d6fc6a806e3b7cba1
manasTyagiGit <manastyagi.ind@gmail.com>
2025-09-07 18:29:24 +0530

    Add GET for all posts

--- app/main_pydantic.py
+++ app/main_pydantic.py
@@ -1,7 +1,11 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
+app = FastAPI()
 
 # Class for Pydantic model which guides the post API body
 class Post (BaseModel) :
@@ -11,8 +15,35 @@ class Post (BaseModel) :
     published_at: Optional[int] = None      # truly optional
 
 
-app = FastAPI()
+# We will also connect to the PostGre server in this file, we use psycopg2 for that
+# DB Connection snippet
 
-@app.get("/")
+conn_try_count = 0
+
+while conn_try_count < 3:
+    try:
+        conn_obj = psycopg2.connect (host= 'localhost', database= 'fastapi', 
+                                user= 'postgres', password= 'password',
+                                cursor_factory= RealDictCursor)
+        
+        cursor = conn_obj.cursor()
+        print ("DB connection successful")
+        break
+
+    except Exception as error:
+        print (f"Connection error: {error}")
+        time.sleep(2)
+        conn_try_count += 1
+
+
+@app.get("/")                       # random test API via base path
 def base_func() :
     return {"status" : "success"}
+
+# R - Read all posts
+@app.get("/posts", status_code= status.HTTP_200_OK)
+def get_posts() :
+    cursor.execute ("SELECT * FROM posts ORDER BY id DESC")
+    posts = cursor.fetchall()
+
+    return {"posts": posts}
