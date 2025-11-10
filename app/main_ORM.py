568976b48f180a12e8a5c9dd6bed332592a0a552
manasTyagiGit <manastyagi.ind@gmail.com>
2025-11-11 05:01:07 +0530

    Add comments

--- app/main_ORM.py
+++ app/main_ORM.py
@@ -1,17 +1,15 @@
-from fastapi import Depends, HTTPException, FastAPI, status, Response
-from .database import engine, get_db
-from typing import List
-from . import models, schemas
-from sqlalchemy.orm import Session
-from . import utils
+from fastapi import FastAPI
+from .database import engine
+from . import models
 from .routers import post, user, auth
 
-
+# app is an object of FastAPI that handles all operations
 app = FastAPI()
 
-#To create all models via the engine in database.py
+# To create all models via the engine in database.py
 models.Base.metadata.create_all(bind=engine)
 
+# including functionality via router files
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
