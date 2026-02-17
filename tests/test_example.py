119686ed633b95697cab53829cbdc6f18627d894
manasTyagiGit <manastyagi.ind@gmail.com>
2026-02-17 09:22:47 +0530

    add pytest parameterize

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,9 +1,18 @@
+import pytest
 from app.example import add, multiply
 
-def test_add() :
+@pytest.mark.parametrize("a, b, res", [
+    (2, 3, 5), (5, 5, 10), (-1, -1, -2)
+])
+def test_add(a, b, res) :
     print ("Testing add function")
-    assert add(8, 5) == 13
+    assert add(a, b) == res
 
-def test_multiply() :
+
+@pytest.mark.parametrize ("a, b, res", [
+    (1, 1, 1), (1, 2, 2), (0, 0, 0),
+    (-1, -1, 1), (-1, 0, 0)
+])
+def test_multiply(a, b, res) :
     print ("Testing multiply function")
-    assert multiply(8, 5) == 40
+    assert multiply(a, b) == res
