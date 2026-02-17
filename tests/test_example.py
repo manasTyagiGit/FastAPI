ad901e8d6990cff832c2582b3f5fbe3963acb735
manasTyagiGit <manastyagi.ind@gmail.com>
2026-02-17 11:00:19 +0530

    Add paramters to test cases for example class

--- tests/test_example.py
+++ tests/test_example.py
@@ -36,15 +36,35 @@ def test_init_bank_account(bank_account) :
 def test_default_init_bank_account(init_default_bank_account) :
     assert init_default_bank_account.balance == 0
 
-def test_withdraw(bank_account) :
-    bank_account.withdraw(69)
-    assert bank_account.balance == 51
+@pytest.mark.parametrize ("withdrawn, bal", [
+    (69, 51), (120, 0)
+])
+def test_withdraw(bank_account, withdrawn, bal) :
+    bank_account.withdraw(withdrawn)
+    assert bank_account.balance == bal
 
-def test_deposit(bank_account):
-    bank_account.deposit(81)
-    assert bank_account.balance == 201
+@pytest.mark.parametrize ("dep, bal", [
+    (81, 201), (22, 142)
+])
+def test_deposit(bank_account, dep, bal):
+    bank_account.deposit(dep)
+    assert bank_account.balance == bal
 
-def test_interest(bank_account):
-    bank_account.collect_interest()
+@pytest.mark.parametrize ("times, bal", [
+    (1, 132), (2, 145), (5, 193)
+])
+def test_interest(bank_account, times, bal):
 
-    assert int(bank_account.balance) == 132
+    for i in range (0, times) :
+        bank_account.collect_interest()
+
+    assert int(bank_account.balance) == bal
+
+@pytest.mark.parametrize ("deposited, withdrawn, remaining", [
+    (100, 100, 0), (120, 50, 70), (212, 75, 137)
+])
+def test_transaction(init_default_bank_account, deposited, withdrawn, remaining) :
+    init_default_bank_account.deposit(deposited)
+    init_default_bank_account.withdraw(withdrawn)
+
+    assert init_default_bank_account.balance == remaining
