85930dd67868fd4f98b881df99936555ee93602e
manasTyagiGit <manastyagi.ind@gmail.com>
2026-02-17 11:09:49 +0530

    Add example.py for understanding pytest

--- app/example.py
+++ app/example.py
@@ -3,3 +3,20 @@ def add (a: int, b: int) -> int :
 
 def multiply (a: int, b: int) -> int :
     return (a * b)
+
+class InsufficientFunds(Exception) :
+    pass
+class BankAccount():
+    def __init__(self, starting_balance=0):
+        self.balance = starting_balance
+
+    def deposit (self, amount) :
+        self.balance += amount
+    
+    def withdraw (self, amount) :
+        if amount > self.balance :
+            raise InsufficientFunds("Insufficient funds in account")
+        self.balance -= amount
+    
+    def collect_interest (self) :
+        self.balance *= 1.1
